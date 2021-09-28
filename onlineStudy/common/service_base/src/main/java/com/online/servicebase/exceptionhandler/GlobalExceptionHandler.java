a9baf5646782fdc040ac441e6c0589b4a7c2459d
worlock <1514866310@qq.com>
2021-09-28 14:39:49 +0800

    统一异常处理配置

---
+++ onlineStudy/common/service_base/src/main/java/com/online/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.online.servicebase.exceptionhandler;
+
+import com.online.commonutils.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/***
+ * 异常处理类
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	/**
+	 * @ExceptionHandler : 指定出现什么异常时执行方法
+	 * 返回result类， 需要在common_base中依赖common_utils
+ 	 */
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	public Result error(Exception e) {
+		e.printStackTrace();
+		return Result.error().message("执行统一异常处理器");
+	}
+}
